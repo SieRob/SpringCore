b25e4d0a404d6a990c877d234d9e7604ad670608
rob <rob.siemons@vdabcampus.be>
2023-05-23 09:34:26 +0200

    Spring Bean short example

---
+++ src/main/java/be/vdab/welkom/repository/LandRepository.java
@@ -0,0 +1,10 @@
+package be.vdab.welkom.repository;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LandRepository {
+    public LandRepository() {
+        System.out.println("LandRepo constructor");
+    }
+}
