2006e34b03a00b8ff2de30061602b590d8c57926
rob <rob.siemons@vdabcampus.be>
2023-05-23 10:29:31 +0200

    Dependency injection

--- src/main/java/be/vdab/welkom/console/MyRunner.java
+++ src/main/java/be/vdab/welkom/console/MyRunner.java
@@ -1,12 +1,24 @@
 package be.vdab.welkom.console;
 
+import be.vdab.welkom.exeptions.RepositoryException;
+import be.vdab.welkom.repository.LandRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements CommandLineRunner {
+    private final LandRepository landRepo;
+    public MyRunner(LandRepository landRepo){
+        this.landRepo = landRepo;
+    }
+
     @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Hello");
+    public void run(String... args) {
+        try {
+            landRepo.findAll()
+                    .forEach(land -> System.out.println(land.getNaam()));
+        }catch (RepositoryException e){
+            e.printStackTrace(System.err);
+        }
     }
 }
